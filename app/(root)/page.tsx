3191fe5d6ceab050ad21625296df23e1dba4c4b9
unknown <minhtu2005ha@gmail.com>
2023-08-17 07:20:14 +0700

    16-08

---
+++ app/(root)/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { useStoreModal } from "@/hooks/use-store-modal"
+import { useEffect } from "react"
+
+
+const SetupPage = () => {
+  const onOpen = useStoreModal(state => state.onOpen)
+  const isOpen = useStoreModal(state => state.isOpen)
+
+  useEffect(() => {
+    if(!isOpen) {
+      onOpen()
+    }
+  }, [isOpen, onOpen])
+  
+  
+  
+  return (
+    <div className='m-4'>
+      <p>Root Page</p>
+    </div> 
+  )
+}
+
+
+export default SetupPage
